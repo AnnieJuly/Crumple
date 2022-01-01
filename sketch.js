8ce2b5809ab24f5582fc480229a31304f1599b5e
AnnieJuly <76923850+AnnieJuly@users.noreply.github.com>
2022-01-01 12:31:51 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+
+var ground;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var engine, world;
+
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+    paper = new Paper(100,640,20);
+
+	left = new Bin(550,625,25,100);
+	bottom = new Bin(610,662,110,25);
+	right = new Bin(670,625,25,100);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  Engine.update(engine);
+
+  paper.display();
+  ground.display();
+
+  left.display();
+  bottom.display();
+  right.display();
+
+  drawSprites();
+ 
+}
+
+function keyPressed(){
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(paper.body,paper.body.position, {x:85,y:-85})
+	}
+}
+
